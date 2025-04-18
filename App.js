1976c728af3afbd71d8ccb97de3c4383888d030f
michael-252 <michaelichukwu252@gmail.com>
2025-04-18 13:20:48 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
+import React, { useState } from "react";
 
 
 function App() {
+const [count, setCount] = useState(0);
+const increment = () => setCount((prevCount) => prevCount + 1);
+const decrement = () => setCount((prevCount) => prevCount - 1);
+
   return (
     <div className="App">
-     <h1> Hello World</h1>
+        <h1> Counter App</h1>
+        <h2>Count: {count}</h2>
+        <button className="btn" onClick={increment}>Increment</button>
+        <button className="btn" onClick={decrement}>Decrement</button>
+        
+
     </div>
   );
 }
